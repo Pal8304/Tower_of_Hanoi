78f0b6793c92e139500f9c405270e21ef82087f9
Pal_314 <4130.stkabirdin@gmail.com>
2024-02-02 23:06:47 +0530

    Implemented dynamic disk count

--- script.js
+++ script.js
@@ -1,3 +1,27 @@
+const disk_count = document.getElementById('disk_count');
+const rod1 = document.getElementById('rod1');
+const base1 = document.getElementById('base1');
+const base2 = document.getElementById('base2');
+const base3 = document.getElementById('base3');
+const colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'];
+
+disk_count.addEventListener('change', function(){
+    const count = parseInt(disk_count.value);
+    while(rod1.firstChild != base1){
+        rod1.removeChild(rod1.firstChild);
+    }
+    for(let i = 0; i < count;i++){
+        const disk = document.createElement('div');
+        disk.classList.add('disk');
+        disk.setAttribute('id', 'disk' + (i+1));
+        disk.setAttribute('draggable', 'true');
+        disk.style.width = base1.offsetWidth - (i+1)*50 + 'px';
+        disk.style.height = '20px';
+        disk.style.backgroundColor = colors[i%(colors.length)];
+        rod1.prepend(disk);
+    }
+})
+
 const tower = document.querySelectorAll('.rod_region');
 const disk = document.querySelectorAll('.disk');
 const move_count = document.getElementById('move_count');
@@ -24,7 +48,7 @@ function dragStart(event){
     const droppingDisk = document.getElementById(event.target.id);
     const rod = droppingDisk.parentElement;
     const topDisk = rod.firstElementChild;
-    console.log("topDisk: ", topDisk);
+    //console.log("topDisk: ", topDisk);
     if(topDisk === droppingDisk){
         droppingDisk.style.opacity = '0.5';
     }
@@ -87,7 +111,7 @@ function checkWin(){
     const tower2 = document.getElementById('rod2').children;
     const tower3 = document.getElementById('rod3').children;
     // console.log("tower2: ", tower2.length);
-    if(tower3.length === 4){
+    if(tower3.length === disk_count+1){
         alert('You win!');
         resetGame();
     }
@@ -97,21 +121,15 @@ function resetGame(){
     const tower1 = document.getElementById('rod1');
     const tower2 = document.getElementById('rod2');
     const tower3 = document.getElementById('rod3');
-    const disk1 = document.getElementById('disk1');
-    const disk2 = document.getElementById('disk2');
-    const disk3 = document.getElementById('disk3');
-    const base1 = document.getElementById('base1');
-    const base2 = document.getElementById('base2');
-    const base3 = document.getElementById('base3');
-    tower1.innerHTML = '';
-    tower2.innerHTML = '';
-    tower3.innerHTML = '';
-    tower1.append(disk1);
-    tower1.append(disk2);
-    tower1.append(disk3);
-    tower1.append(base1);
-    tower2.append(base2);
-    tower3.append(base3);
+    const disks = document.querySelectorAll('.disk');
+    disks_array = [].slice.call(disks);
+    disks_array.sort((a,b) => {
+        return b.offsetWidth - a.offsetWidth;
+    });
+    // console.log("disks: ", disks);
+    for(let i = 0; i < disk_count;i++){
+        tower1.prepend(disks_array[i]);
+    }
     move_count.innerHTML = 0;
 }
 
